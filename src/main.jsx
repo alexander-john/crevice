7bf344e53e5699e2202cf0d3b6609ebd1a023ad4
Alexander John <96aljohn@gmail.com>
2024-09-09 19:51:42 -0700

    integrate-apollo-client

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import App from './App.jsx'
 import './index.css'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const client = new ApolloClient({
+    uri: 'https://flyby-router-demo.herokuapp.com/',
+    cache: new InMemoryCache(),
+});
+
+// const client = ...
+
+client
+    .query({
+        query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+    })
+    .then((result) => console.log(result));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+);
